cfdf13e7c32b42725ef11d0956bcf7aa44bdc8c0
Zakisee <1210143978@qq.com>
2022-05-18 17:05:24 +0800

    feat: element挂载流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -2,7 +2,15 @@ import { h } from '../../lib/soft-vue.esm.js'
 export const App = {
   render() {
     // ui
-    return h('div', 'hi, ' + this.msg)
+    return h(
+      'div',
+      { class: ['red', 'green'], id: 'fun' },
+      // 'hi, ' + this.msg
+      [
+        h('div', { class: 'red' }, '111'),
+        h('div', { class: 'green' }, [h('b', {}, '333')])
+      ]
+    )
   },
 
   setup() {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from '../shared/index'
 import { createComponentInstance, setupComponent } from './component'
 
 export function render(vnode, container) {
@@ -5,11 +6,40 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  // TODO:
-  // processElement(vnode, container)
-  processComponent(vnode, container)
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container)
+  } else if (isObject(vnode)) {
+    processComponent(vnode, container)
+  }
+}
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container)
 }
 
+// 初始化element
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type)
+  const { children, props } = vnode
+  // handle children
+  if (typeof children === 'string') {
+    // 字符串说明是文本节点
+    el.textContent = vnode.children
+  } else if (Array.isArray(children)) {
+    mountChildren(children, el)
+  }
+  //hanle props
+  for (let key in props) {
+    const val = props[key]
+    el.setAttribute(key, val)
+  }
+  container.appendChild(el)
+}
+
+function mountChildren(children, el) {
+  children.forEach((e) => {
+    patch(e, el)
+  })
+}
 function processComponent(vnode: any, container: any) {
   mountComponent(vnode, container)
 }
