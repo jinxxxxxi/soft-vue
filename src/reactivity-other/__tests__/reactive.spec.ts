e37745d8297820901f8b18f7f224e4a34c3e1e7d
Zakisee <1210143978@qq.com>
2022-06-30 20:48:39 +0800

    refactor: isReactive & iaReadlone

--- src/reactivity-other/__tests__/reactive.spec.ts
+++ src/reactivity-other/__tests__/reactive.spec.ts
@@ -1,18 +1,18 @@
 // import { reactive, isReactive, toRaw, reactiveMap } from "../src/reactive";
-import { reactive } from '../index'
+import { reactive, isReactive } from '../index'
 describe('reactive', () => {
   test('Object', () => {
     const original = { foo: 1 }
     const observed = reactive(original)
     expect(observed).not.toBe(original)
-    // expect(isReactive(observed)).toBe(true);
-    // expect(isReactive(original)).toBe(false);
+    expect(isReactive(observed)).toBe(true)
+    expect(isReactive(original)).toBe(false)
     // // get
-    // expect(observed.foo).toBe(1);
+    expect(observed.foo).toBe(1)
     // //     // has
-    // expect("foo" in observed).toBe(true);
+    expect('foo' in observed).toBe(true)
     // //     // ownKeys
-    // expect(Object.keys(observed)).toEqual(["foo"]);
+    expect(Object.keys(observed)).toEqual(['foo'])
   })
 
   //   test("nested reactives", () => {
--- src/reactivity-other/reactive.ts
+++ src/reactivity-other/reactive.ts
@@ -1,10 +1,28 @@
+const ReactiveStatus = {
+  isReactive: '__v__is_reactive',
+  isReadonly: '__v__is_readonly'
+}
+
 export const reactive = (target) => {
   return new Proxy(target, {
     get: function (target, key) {
+      if (key === ReactiveStatus.isReactive) {
+        return true
+      }
+      if (key === ReactiveStatus.isReadonly) {
+        return true
+      }
       return Reflect.get(target, key)
     },
     set: function (target, key, value) {
       return Reflect.set(target, key, value)
     }
   })
+}
+
+export const isReactive = (obj) => {
+  return !!obj[ReactiveStatus.isReactive]
+}
+export const isReadonly = (obj) => {
+  return !!obj[ReactiveStatus.isReadonly]
 }
